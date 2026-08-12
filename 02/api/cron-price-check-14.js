93aaab02e62a0ec96f1008dd9190ec46226f4f08
Jong Oun Kim <jokim@spigen.com>
2026-08-12 16:34:41 -0700

    fix(02): Cron ESM 로딩 오류 수정 (TODO: Phase 3 완료)

--- 02/api/cron-price-check-14.js
+++ 02/api/cron-price-check-14.js
@@ -1,3 +1,4 @@
-import handler from './price-check.mjs';
-
-export default handler;
+export default async function handler(req, res) {
+  const { default: priceCheck } = await import('./price-check.mjs');
+  return priceCheck(req, res);
+}
